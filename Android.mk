721a0d190465c2ee47b7e03d6019c655c08091b9
cretin45 <cretin45@gmail.com>
2013-06-10 16:07:04 -0700

    Only call src dir.
    Change-Id: I91514e23b8fcdcf0520dad4a32b585ddeb7a9493

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
@@ -15,7 +15,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 
 include $(BUILD_PACKAGE)
-include $(CLEAR_VARS)
+
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := gson:libs/gson-2.2.4.jar volley-v14:libs/volley.jar
 
 include $(BUILD_MULTI_PREBUILT)
