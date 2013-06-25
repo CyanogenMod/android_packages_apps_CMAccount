bc0180e81ce658e0561055f77d0565cb6e662cf7
cretin45 <cretin45@gmail.com>
2013-06-25 15:33:31 -0700

    Add Google Play dependencies.
    Change-Id: Ia0c00e0707f459bf0581d124d8be3749c2c20611

--- Android.mk
+++ Android.mk
@@ -7,16 +7,26 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := CMID
 
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
     volley-v14 \
-    gson
+    gson \
+    play
 
+# Include res dir from chips
+google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
+res_dir := $(google_play_dir) res
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.google.android.gms
 
 include $(BUILD_PACKAGE)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := gson:libs/gson-2.2.4.jar volley-v14:libs/volley.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := gson:libs/gson-2.2.4.jar volley-v14:libs/volley.jar play:../../../external/google/google_play_services/libproject/google-play-services_lib/libs/google-play-services.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
