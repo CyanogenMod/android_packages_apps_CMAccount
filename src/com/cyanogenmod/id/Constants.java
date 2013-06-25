3fa08f7907408715101622beb0acb9c96ba2a49d
cretin45 <cretin45@gmail.com>
2013-06-25 15:27:44 -0700

    Added some global constants.
    Change-Id: I38b42a36401d614b61c4027b2a426cece54cc4cc

--- src/com/cyanogenmod/id/Constants.java
+++ src/com/cyanogenmod/id/Constants.java
@@ -2,6 +2,7 @@ package com.cyanogenmod.id;
 
 public class Constants {
 
+    public static final String TAG = "CMID";
     public static final boolean DEBUG = true;
 
     public static final String ACCOUNT_TYPE = "com.cyanogenmod.id";
@@ -15,5 +16,15 @@ public class Constants {
     public static final String EXTRA_FIRST_RUN = "firstRun";
     public static final String EXTRA_SHOW_BUTTON_BAR = "extra_prefs_show_button_bar";
     public static final String EXTRA_SHOW_WIFI_MENU = "wifi_show_menus";
+
+    public static final String GCM_PREFERENCES = "com.cyanogenmod.id.gcm";
+    public static final String AUTH_PREFERENCES = "com.cyanogenmod.id.auth";
+    public static final String SETTINGS_PREFERENCES = "com.cyanogenmod.id_preferences";
+
+    public static final String KEY_FIND_DEVICE_PREF = "find_device";
+
+    public static final String BACKOFF_MS = "backoff_ms";
+    public static final int DEFAULT_BACKOFF_MS = 3000;
+    public static final int MAX_BACKOFF_MS = 1000 * 60 * 60; // 1 hour
 
 }
