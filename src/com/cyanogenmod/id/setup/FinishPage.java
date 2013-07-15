afa3669fff32876aacce5747b4279ff5919a3af9
cretin45 <cretin45@gmail.com>
2013-07-14 20:21:56 -0700

    Clean up imports.

--- src/com/cyanogenmod/id/setup/FinishPage.java
+++ src/com/cyanogenmod/id/setup/FinishPage.java
@@ -6,10 +6,6 @@ import com.cyanogenmod.id.ui.SetupPageFragment;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class FinishPage extends Page {
 
