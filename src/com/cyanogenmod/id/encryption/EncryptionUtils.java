f244157d83a1d2c9addc90071c494309ae8bc698
Chris Soyars <ctso@ctso.me>
2013-08-01 22:04:12 -0700

    Add AES decrypt helpers

--- src/com/cyanogenmod/id/encryption/EncryptionUtils.java
+++ src/com/cyanogenmod/id/encryption/EncryptionUtils.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import com.cyanogenmod.id.CMID;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
@@ -21,8 +23,39 @@ public class EncryptionUtils {
                 byte[] symmetricKey = keyGenerator.generateKey().getEncoded();
                 return Base64.encodeToString(symmetricKey, Base64.DEFAULT).replace("\n", "");
             } catch (NoSuchAlgorithmException e) {
-                Log.d(TAG, "NoSuchAlgorithimException", e);
+                Log.e(TAG, "NoSuchAlgorithimException", e);
             }
+            return null;
+        }
+
+        public static String decrypt(String _ciphertext, String _key, String _iv) {
+            byte[] key = Base64.decode(_key, Base64.DEFAULT);
+            byte[] iv = Base64.decode(_iv, Base64.DEFAULT);
+            byte[] ciphertext = Base64.decode(_ciphertext, Base64.DEFAULT);
+
+            SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+                byte[] plaintext = cipher.doFinal(ciphertext);
+
+                return new String(plaintext);
+            } catch (NoSuchAlgorithmException e) {
+                Log.e(TAG, "NoSuchAlgorithimException", e);
+            } catch (NoSuchPaddingException e) {
+                Log.e(TAG, "NoSuchPaddingException", e);
+            } catch (InvalidKeyException e) {
+                Log.e(TAG, "InvalidKeyException", e);
+            } catch (IllegalBlockSizeException e) {
+                Log.e(TAG, "IllegalBlockSizeException", e);
+            } catch (BadPaddingException e) {
+                Log.e(TAG, "BadPaddingException", e);
+            } catch (InvalidAlgorithmParameterException e) {
+                Log.e(TAG, "InvalidAlgorithmParameterException", e);
+            }
+
             return null;
         }
     }
@@ -35,9 +68,9 @@ public class EncryptionUtils {
                 KeyFactory keyFactory = KeyFactory.getInstance("RSA");
                 return keyFactory.generatePublic(new X509EncodedKeySpec(Base64.decode(publicKey.toString(), Base64.DEFAULT)));
             } catch (NoSuchAlgorithmException e) {
-                Log.d(TAG, "NoSuchAlgorithimException", e);
+                Log.e(TAG, "NoSuchAlgorithimException", e);
             } catch (InvalidKeySpecException e) {
-                Log.d(TAG, "InvalidKeySpecException", e);
+                Log.e(TAG, "InvalidKeySpecException", e);
             }
             return null;
         }
