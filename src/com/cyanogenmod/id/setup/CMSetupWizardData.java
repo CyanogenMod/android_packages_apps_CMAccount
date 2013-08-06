5fb53f47bd78a2d73f9b436fbfb2349e71811b54
cretin45 <cretin45@gmail.com>
2013-08-06 10:50:03 -0700

    Remove SIM missing page. This should be a warning dialog.

--- src/com/cyanogenmod/id/setup/CMSetupWizardData.java
+++ src/com/cyanogenmod/id/setup/CMSetupWizardData.java
@@ -13,7 +13,6 @@ public class CMSetupWizardData extends AbstractSetupData {
     @Override
     protected PageList onNewPageList() {
         return new PageList(new WelcomePage(mContext, this, R.string.setup_welcome, R.drawable.cid_welcome),
-               new SimMissingPage(mContext, this, R.string.setup_sim_missing, R.drawable.cid_confused),
                new CMIDPage(mContext, this, R.string.setup_cmid, R.drawable.cid_cmid),
                new GoogleAccountPage(mContext, this, R.string.setup_google_account, R.drawable.cid_google),
                new LocationSettingsPage(mContext, this, R.string.setup_location),
