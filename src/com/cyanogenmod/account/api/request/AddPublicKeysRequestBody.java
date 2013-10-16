ade6ad99586cd71ad08ff9632f382e9935b03255
Chris Soyars <ctso@ctso.me>
2013-10-16 15:06:09 -0700

    Precompute public keys hash
    Right now the server is unnecessiarly computing this hash.  We can save
    significant server resources by moving this computation to the client.
    
    Change-Id: Ibb72ea782a2e34ac375a255e65262f89e1367712

--- src/com/cyanogenmod/account/api/request/AddPublicKeysRequestBody.java
+++ src/com/cyanogenmod/account/api/request/AddPublicKeysRequestBody.java
@@ -23,16 +23,22 @@ import com.google.gson.Gson;
 
 import org.spongycastle.crypto.params.ECPublicKeyParameters;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class AddPublicKeysRequestBody {
-    private List<PublicKey> public_keys;
+    private LinkedList<PublicKey> public_keys;
+    private String public_keys_hash;
     private String device_id;
 
     public AddPublicKeysRequestBody(Context context, List<ECKeyPair> keyPairs) {
         device_id = CMAccountUtils.getUniqueDeviceId(context);
-        public_keys = new ArrayList<PublicKey>();
+        public_keys = new LinkedList<PublicKey>();
         byte[] hmacSecret = CMAccountUtils.getHmacSecret(context);
 
         for (ECKeyPair keyPair : keyPairs) {
@@ -41,6 +47,25 @@ public class AddPublicKeysRequestBody {
             String signature = calculateSignature(publicKey, hmacSecret);
 
             public_keys.add(new PublicKey(publicKey, key_id, signature));
+        }
+
+        public_keys_hash = generatePublicKeysHash();
+    }
+
+    private String generatePublicKeysHash() {
+        Collections.sort(public_keys);
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            for (PublicKey publicKey : public_keys) {
+                md.update(publicKey.getKeyId().getBytes());
+                md.update(publicKey.getSignature().getBytes());
+                md.update(publicKey.getPublicKey().getBytes());
+            }
+
+            return CMAccountUtils.encodeHex(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError(e);
         }
     }
 
@@ -53,7 +78,7 @@ public class AddPublicKeysRequestBody {
         return gson.toJson(this);
     }
 
-    private static class PublicKey {
+    private static class PublicKey implements Comparable<PublicKey> {
         private String public_key;
         private String key_id;
         private String signature;
@@ -62,6 +87,23 @@ public class AddPublicKeysRequestBody {
             this.public_key = CMAccountUtils.encodeHex(publicKey.getQ().getEncoded());
             this.key_id = key_id;
             this.signature = signature;
+        }
+
+        public String getPublicKey() {
+            return public_key;
+        }
+
+        public String getKeyId() {
+            return key_id;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+
+        @Override
+        public int compareTo(PublicKey another) {
+            return key_id.compareTo(another.getKeyId());
         }
     }
 }
