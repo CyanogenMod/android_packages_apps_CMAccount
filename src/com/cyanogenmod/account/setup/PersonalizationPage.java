86a00cbeb5496cb5387c17102f759d0c06dfbb07
Roman Birg <roman@cyngn.com>
2014-05-28 14:04:08 -0700

    CMAccount: don't crash when toggling nav keys quickly
    Same symptom appears in Settings. Avoid the race condition
    by adding a slight delay.
    
    Change-Id: Ifea43d3281dd49cd8f428d6e7c2bb0a229939186
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/cyanogenmod/account/setup/PersonalizationPage.java
+++ src/com/cyanogenmod/account/setup/PersonalizationPage.java
@@ -24,6 +24,7 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.provider.Settings;
 import android.view.IWindowManager;
@@ -70,8 +71,10 @@ public class PersonalizationPage extends Page {
     public static class PersonalizationFragment extends SetupPageFragment {
 
         ViewSwitcher mSwitcher;
+        Handler mHandler;
 
         public PersonalizationFragment() {
+            mHandler = new Handler();
         }
 
         @Override
@@ -132,11 +135,27 @@ public class PersonalizationPage extends Page {
                 useNavBar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        writeDisableNavkeysOption(getActivity(), isChecked);
+                        mHandler.removeCallbacks(mDisableNavKeysRunnable);
+                        mHandler.removeCallbacks(mEnableNavKeysRunnable);
+                        mHandler.postDelayed(isChecked ? mEnableNavKeysRunnable : mDisableNavKeysRunnable, 500);
                     }
                 });
             }
         }
+
+        private Runnable mDisableNavKeysRunnable = new Runnable() {
+            @Override
+            public void run() {
+                writeDisableNavkeysOption(getActivity(),  false);
+            }
+        };
+
+        private Runnable mEnableNavKeysRunnable = new Runnable() {
+            @Override
+            public void run() {
+                writeDisableNavkeysOption(getActivity(),  true);
+            }
+        };
 
         @Override
         protected int getLayoutResource() {
