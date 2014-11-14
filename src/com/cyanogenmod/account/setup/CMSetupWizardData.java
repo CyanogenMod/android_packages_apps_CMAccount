b4057fb9ba5d69b5edcbb131cf5fba1136e2b4e4
cretin45 <cretin45@gmail.com>
2014-11-14 14:33:21 -0800

    SetupWizard: Give option to turn on mobile data if off by default
    Change-Id: I6196e15e7e3b244115b6a2b25f8b8f9afc8f70d8

--- src/com/cyanogenmod/account/setup/CMSetupWizardData.java
+++ src/com/cyanogenmod/account/setup/CMSetupWizardData.java
@@ -17,8 +17,11 @@
 package com.cyanogenmod.account.setup;
 
 import com.cyanogenmod.account.R;
+import com.cyanogenmod.account.util.CMAccountUtils;
 
 import android.content.Context;
+
+import java.util.ArrayList;
 
 public class CMSetupWizardData extends AbstractSetupData {
 
@@ -28,14 +31,19 @@ public class CMSetupWizardData extends AbstractSetupData {
 
     @Override
     protected PageList onNewPageList() {
-        return new PageList(new WelcomePage(mContext, this, R.string.setup_welcome),
-               new GoogleAccountPage(mContext, this, R.string.setup_google_account),
-               new CMAccountPage(mContext, this, R.string.setup_cmaccount),
-               new LocationSettingsPage(mContext, this, R.string.setup_location),
-               new PersonalizationPage(mContext, this, R.string.setup_personalization),
-               new DateTimePage(mContext, this, R.string.setup_datetime),
-               new FinishPage(mContext, this, R.string.setup_complete)
-        );
+        ArrayList<Page> pages = new ArrayList<Page>();
+        pages.add(new WelcomePage(mContext, this, R.string.setup_welcome));
+        if (CMAccountUtils.hasTelephony(mContext) &&
+                !CMAccountUtils.isMobileDataEnabled(mContext)) {
+            pages.add(new MobileDataPage(mContext, this, R.string.setup_mobile_data));
+        }
+        pages.add(new GoogleAccountPage(mContext, this, R.string.setup_google_account));
+        pages.add(new CMAccountPage(mContext, this, R.string.setup_cmaccount));
+        pages.add(new LocationSettingsPage(mContext, this, R.string.setup_location));
+        pages.add(new PersonalizationPage(mContext, this, R.string.setup_personalization));
+        pages.add(new DateTimePage(mContext, this, R.string.setup_datetime));
+        pages.add(new FinishPage(mContext, this, R.string.setup_complete));
+        return new PageList(pages.toArray(new Page[pages.size()]));
     }
 
 
