e8b52c93382ff3e8ef44da511f704f4330749423
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-09 19:21:42 -0800

    Allow as SetupWizard only
    OSS-59
    
    Change-Id: If32cfe0459385604c9b63aa982178da9aabc00d6

--- src/com/cyanogenmod/account/setup/CMSetupWizardData.java
+++ src/com/cyanogenmod/account/setup/CMSetupWizardData.java
@@ -30,7 +30,6 @@ public class CMSetupWizardData extends AbstractSetupData {
     protected PageList onNewPageList() {
         return new PageList(new WelcomePage(mContext, this, R.string.setup_welcome),
                new GoogleAccountPage(mContext, this, R.string.setup_google_account),
-               new CMAccountPage(mContext, this, R.string.setup_cmaccount),
                new LocationSettingsPage(mContext, this, R.string.setup_location),
                new PersonalizationPage(mContext, this, R.string.setup_personalization),
                new DateTimePage(mContext, this, R.string.setup_datetime),
